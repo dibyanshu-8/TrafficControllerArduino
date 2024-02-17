4e68b7e027346ac4ac0fa73e4707d81822044022
Dibyanshu kar <136263029+dibyanshu-8@users.noreply.github.com>
2024-02-17 15:38:16 +0530

    Update traffic_controller1.ino

--- traffic_controller1.ino
+++ traffic_controller1.ino
@@ -1,18 +1,18 @@
-int red = 9; //constant variable named red and assigns it the value 9, indicating that digital pin 9 on the Arduino board will be used to control the red LED.
-int yellow = 8;
-int green = 7;
+int red = 11; //constant variable named red and assigns it the value 11, indicating that digital pin 11 on the Arduino board will be used to control the red LED.
+int yellow = 12;
+int green = 13;
 
 void setup(){
   
-  pinMode(red, OUTPUT); //This line configures digital pin 9 (red) as an output pin, meaning it can send voltage (HIGH) or no voltage (LOW) to control the LED's on/off state.
+  pinMode(red, OUTPUT); //This line configures digital pin 11 (red) as an output pin, meaning it can send voltage (HIGH) or no voltage (LOW) to control the LED's on/off state.
   pinMode(yellow, OUTPUT);
   pinMode(green,  OUTPUT);
   
 }
 void loop(){
-digitalWrite(red, HIGH); //Turns on the red LED by setting pin 9 to HIGH (5 volts).
+digitalWrite(red, HIGH); //Turns on the red LED by setting pin 11 to HIGH (5 volts).
  delay(15000); // Waits for 15 seconds using the delay() function.
-digitalWrite(red,  LOW); //Turns off the red LED by setting pin 9 to LOW (0 volts).
+digitalWrite(red,  LOW); //Turns off the red LED by setting pin 11 to LOW (0 volts).
 
   //This block of code (turning on yellow for 1 second, off for 0.5 second) is repeated five times, resulting in a 5-second flashing yellow sequence.
   digitalWrite(yellow, HIGH);
