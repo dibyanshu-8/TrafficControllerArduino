024d37cba9a67b20d5753672bbc1d827b4b4835f
Dibyanshu kar <136263029+dibyanshu-8@users.noreply.github.com>
2024-02-17 15:29:51 +0530

    Update traffic_controller1.ino

--- traffic_controller1.ino
+++ traffic_controller1.ino
@@ -13,11 +13,12 @@ void loop(){
 digitalWrite(red, HIGH); //Turns on the red LED by setting pin 9 to HIGH (5 volts).
  delay(15000); // Waits for 15 seconds using the delay() function.
 digitalWrite(red,  LOW); //Turns off the red LED by setting pin 9 to LOW (0 volts).
-  
+
+  //This block of code (turning on yellow for 1 second, off for 0.5 second) is repeated five times, resulting in a 5-second flashing yellow sequence.
   digitalWrite(yellow, HIGH);
 delay(1000); //Waits for 1 second.
   digitalWrite(yellow,  LOW);
-delay(500);
+delay(500); //Waits for 0.5 seconds.
 
   digitalWrite(yellow, HIGH);
 delay(1000);
@@ -40,9 +41,10 @@ delay(1000);
 delay(500);
   
 digitalWrite(green, HIGH);
-delay(20000);
+delay(20000); //Waits for 20 seconds (longer green duration).
 digitalWrite(green,  LOW);
-//  
+
+//The yellow flashing sequence is repeated again.
 digitalWrite(yellow, HIGH);
 delay(1000);
   digitalWrite(yellow,  LOW);
